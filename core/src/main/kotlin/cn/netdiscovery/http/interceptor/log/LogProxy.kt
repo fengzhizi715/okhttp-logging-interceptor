440f3d8c35976f0d55a773457d5d97bbad93d74c
fengzhizi715 <fengzhizi715@126.com>
2020-09-12 13:52:18 +0800

    update kotlin's versio

--- core/src/main/kotlin/cn/netdiscovery/http/interceptor/log/LogProxy.kt
+++ core/src/main/kotlin/cn/netdiscovery/http/interceptor/log/LogProxy.kt
@@ -6,7 +6,7 @@ package cn.netdiscovery.http.interceptor.log
  *          cn.netdiscovery.http.interceptor.log.LogProxy
  * @author: Tony Shen
  * @date: 2020-08-14 17:56
- * @version: V1.0 日志库的代理操作，便于开发者使用自己的日志库
+ * @version: V1.0 日志库的代理操作，便于开发者使用自己的日志库，这样可以支持 Android、Java 项目都使用它
  */
 interface LogProxy {
 
